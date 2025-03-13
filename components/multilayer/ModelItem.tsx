26fdf8bb6f5ac0215056ab407140dde5318c704b
Dr Alexander P Cave <alex.cave@outlook.com.au>
2025-03-13 21:12:44 +1100

    Models page working. Altering documents for css then boycot tailwindcss in production.

--- components/multilayer/ModelItem.tsx
+++ components/multilayer/ModelItem.tsx
@@ -1,19 +1,23 @@
 import { iModel } from "@/lib/data-helpers";
-import AcousticChart from "@/components/multilayer/AcousticChart";
-import { redirect } from "next/navigation";
+import ChartPreview from "@/components/multilayer/ChartPreview";
+import { useRouter } from "next/navigation";
+
+import "@/components/multilayer/ModelItem.css";
 
 export default function ModelItem({ model }: { model: iModel }) {
+  const router = useRouter();
+
   function handleClick() {
     console.log(`Clicked on model: ${model.name}`);
 
     // As this is a server component search engines can index these links.
-    redirect(`/models/${model._id}`);
+    router.push(`/acoustic/models/${model._id}`);
   }
 
   return (
-    <div className="model-item" onClick={handleClick}>
-      <h2>{model.name}</h2>
-      <AcousticChart />
+    <div id="model-item" onClick={handleClick}>
+      <h3>{model.name}</h3>
+      <ChartPreview />
       <p>{model.description}</p>
     </div>
   );
--- components/multilayer/ModelList.tsx
+++ components/multilayer/ModelList.tsx
@@ -2,9 +2,11 @@
 import ModelItem from "@/components/multilayer/ModelItem";
 import { iModel } from "@/lib/data-helpers";
 
+import "@/components/multilayer/ModelList.css";
+
 export default function ModelList({ models }: { models: iModel[] }) {
   return (
-    <div className="model-list">
+    <div id="model-list">
       {models.map((model) => (
         <ModelItem key={model._id} model={model} />
       ))}
