5953e4ed975407ffbdd4d50b7321b6134b37e863
Dr Alexander P Cave <alex.cave@outlook.com.au>
2025-03-13 22:38:20 +1100

    Resume reformatted, yet to do the landing page.

--- components/documents/Page.jsx
+++ components/documents/Page.jsx
@@ -1,104 +1,96 @@
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
+
+import "@/components/documents/Page.css";
 
 export function WebContent({ name, text, children, ...props }) {
-
-    return (
-        <div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 1 }}
-
-        className="w-[170mm] bg-white text-black shadow-md font-serif mx-auto p-8 rounded m-[5mm] relative">
-           <h1 className="text-4xl text-center">{name}</h1>
-           <p className="text-sm text-center mt-2 mb-2">{text}</p>
-        {children}
-        </div>
-    );
+  return (
+    <div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1 }}
+      className="w-[170mm] bg-white text-black shadow-md font-serif mx-auto p-8 rounded m-[5mm] relative"
+    >
+      <h1 className="text-4xl text-center">{name}</h1>
+      <p className="text-sm text-center mt-2 mb-2">{text}</p>
+      {children}
+    </div>
+  );
 }
 
-export function Page({ name, text, numPages, pageNum, onPageSelect, children, ...props }) {
+export function Page({
+  name,
+  text,
+  numPages,
+  pageNum,
+  onPageSelect,
+  children,
+  ...props
+}) {
+  let navElements = [];
 
-    let navElements = [];
-
-    if (numPages > 1) {
-        for (let i = 0; i < numPages; i++) {
-            console.log('test:',i);
-            navElements.push(<button key={i}
-                            className={`text-sm ${pageNum === (i + 1) && ' underline'} hover:text-sky-400`}
-                            onClick={() => onPageSelect(i+1)}>
-                            Page {i+1}
-                            </button>    
-                        );
-        }
+  if (numPages > 1) {
+    for (let i = 0; i < numPages; i++) {
+      console.log("test:", i);
+      navElements.push(
+        <button
+          key={i}
+          className={`${pageNum === i + 1 && "current"}`}
+          onClick={() => onPageSelect(i + 1)}
+        >
+          Page {i + 1}
+        </button>
+      );
     }
+  }
 
-
-    return (
-        <motion.div
+  return (
+    <div id="page" className="frame">
+      <motion.div
+        className="paper"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 1 }}
-
-        className="w-[170mm] h-[257mm] bg-white text-black shadow-md font-serif mx-auto p-8 rounded m-[5mm] relative">
-           <h1 className="text-4xl text-center">{name}</h1>
-           <p className="text-sm text-center mt-2 mb-2">{text}</p>
+      >
+        <h1>{name}</h1>
+        <h3 className="centre-text">{text}</h3>
         {children}
-        <div className="flex justify-center items-center mt-4 space-x-4 absolute bottom-4 left-0 w-full">
-            {navElements}
-        </div>
-        </motion.div>
-    );
+        <div className="paper-nav">{navElements}</div>
+      </motion.div>
+    </div>
+  );
 }
 
-export function Paragraph({ children, ...props }) {
-    return (
-        <p className="text-xs" {...props}>
-        {children}
-        </p>
-    );
-}
-
-export function Heading({ text, children, ...props }) {
-    return (
-        <>
-        <h2 className="text-sm mt-1 font-bold" {...props}>
-        {text}
-        </h2>
-        <hr className="border-t border-black mb-2" />
-        {children}
-        </>
-    );
+export function Heading({ text, children }) {
+  return (
+    <>
+      <h2>{text}</h2>
+      <hr />
+      {children}
+    </>
+  );
 }
 
 export function NamedList({ name, children, ...props }) {
-    return (
-        <>
-        <h3 className="text-xs mt-2 font-bold">{name}</h3>
-        <List {...props}>{children}</List>
-        </>
-    );
-}
-
-export function List({ children, ...props }) {
-    return (
-        <ul className="list-disc ml-6 text-xs" {...props}>
-        {children}
-        </ul>
-    );
+  return (
+    <>
+      <h3>{name}</h3>
+      <ul className="dots">{children}</ul>
+    </>
+  );
 }
 
 export function Entry({ title, leftText, rightText, children, ...props }) {
-    return (
-        <>
-        <div className="flex justify-between items-center text-xs mt-1 mb-1">
-            <h3>
-                <strong>{title}</strong>, {leftText}
-            </h3>
-            <span>{rightText}</span>
-        </div>
-        {children}
-        </>
-    );
+  return (
+    <>
+      <div className="entry">
+        <h3>
+          <strong>{title}</strong>, {leftText}
+        </h3>
+        <span>{rightText}</span>
+      </div>
+      {children}
+    </>
+  );
 }
