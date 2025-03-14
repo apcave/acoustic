11a39c929274cb1bb6189e39fdaaecb42df04bea
Dr Alexander P Cave <alex.cave@outlook.com.au>
2025-03-14 15:25:18 +1100

    Added google analytics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/Header";
-
 import { Provider as AuthProvider } from "./provider";
 import "./globals.css";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -10,9 +10,23 @@ export default function RootLayout({
 }>) {
   return (
     <AuthProvider>
-      <Header />
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          <Header />
+          {children}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-M5C46MC755"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-M5C46MC755');
+            `}
+          </Script>
+        </body>
       </html>
     </AuthProvider>
   );
