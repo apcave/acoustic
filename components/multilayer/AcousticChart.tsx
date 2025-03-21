425bd3dc2f753cc2e2382253095cbf5d91d84208
Dr Alexander P Cave <alex.cave@outlook.com.au>
2025-03-21 12:25:37 +1100

    More graphs added, building UI.

--- components/multilayer/AcousticChart.tsx
+++ components/multilayer/AcousticChart.tsx
@@ -13,6 +13,24 @@ import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
 import { iResult, iSweep } from "@/lib/data-helpers";
 
+function unwrap(angles: number[]): number[] {
+  let unwrapped = [...angles];
+  const threshold = Math.PI;
+  let offset = 0;
+
+  for (let i = 1; i < angles.length; i++) {
+    const delta = angles[i] - angles[i - 1];
+    if (delta > threshold) {
+      offset -= 2 * Math.PI;
+    } else if (delta < -threshold) {
+      offset += 2 * Math.PI;
+    }
+    unwrapped[i] += offset;
+  }
+  unwrapped = unwrapped.map((angle) => (angle * 180) / Math.PI);
+  return unwrapped;
+}
+
 const AcousticChart: React.FC = () => {
   const results = useSelector(
     (state: RootState) => state.model.model.results
@@ -28,6 +46,27 @@ const AcousticChart: React.FC = () => {
   if (!sweep) {
     return null;
   }
+  const unwrappedRpTheta = unwrap(
+    results.Rp.imag.map((_, index) =>
+      Math.atan2(results.Rp.imag[index], results.Rp.real[index])
+    )
+  );
+  console.log("unwrappedRpTheta", unwrappedRpTheta);
+  const unwrappedTpTheta = unwrap(
+    results.Tp.imag.map((_, index) =>
+      Math.atan2(results.Tp.imag[index], results.Tp.real[index])
+    )
+  );
+  const unwrappedRsTheta = unwrap(
+    results.Rs.imag.map((_, index) =>
+      Math.atan2(results.Rs.imag[index], results.Rs.real[index])
+    )
+  );
+  const unwrappedTsTheta = unwrap(
+    results.Ts.imag.map((_, index) =>
+      Math.atan2(results.Ts.imag[index], results.Ts.real[index])
+    )
+  );
 
   const data = sweep.values.map((val, index) => {
     const Rp_abs = Math.sqrt(
@@ -36,26 +75,40 @@ const AcousticChart: React.FC = () => {
     const Tp_abs = Math.sqrt(
       results.Tp.real[index] ** 2 + results.Tp.imag[index] ** 2
     );
+    const Rs_abs = Math.sqrt(
+      results.Rs.real[index] ** 2 + results.Rs.imag[index] ** 2
+    );
+    const Ts_abs = Math.sqrt(
+      results.Ts.real[index] ** 2 + results.Ts.imag[index] ** 2
+    );
     return {
       values: sweep.values[index],
       Rp_r: results.Rp.real[index],
       Rp_i: results.Rp.imag[index],
       "abs(Rp)": Rp_abs,
-      Rp_theta: Math.atan2(results.Rp.imag[index], results.Rp.real[index]),
+      "theta(Rp)": unwrappedRpTheta[index],
       Rp_db: 20 * Math.log10(Rp_abs),
       Rs_r: results.Rs.real[index],
       Rs_i: results.Rs.imag[index],
-      "abs(Tp)": Tp_abs,
+      "abs(Rs)": Rs_abs,
+      "theta(Rs)": unwrappedRsTheta[index],
+      Rs_db: 20 * Math.log10(Rs_abs),
       Tp_r: results.Tp.real[index],
       Tp_i: results.Tp.imag[index],
+      "abs(Tp)": Tp_abs,
+      "theta(Tp)": unwrappedTpTheta[index],
       Ts_r: results.Ts.real[index],
       Ts_i: results.Ts.imag[index],
+      "abs(Ts)": Ts_abs,
+      "theta(Ts)": unwrappedTsTheta[index],
+      Ts_db: 20 * Math.log10(Ts_abs),
       TsE: results.TsE[index],
       TpE: results.TpE[index],
       RpE: results.RpE[index],
       RsE: results.RsE[index],
     };
   });
+  console.log("data", data);
 
   // Calculate the range of x values
   const xRange = Math.max(...sweep.values) - Math.min(...sweep.values);
@@ -118,6 +171,177 @@ const AcousticChart: React.FC = () => {
           <Legend />
           <Line type="monotone" dataKey="abs(Rp)" stroke="red" dot={false} />
           <Line type="monotone" dataKey="abs(Tp)" stroke="blue" dot={false} />
+        </LineChart>
+      </ResponsiveContainer>
+      <ResponsiveContainer width="100%" height={300}>
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="values"
+            tickFormatter={formatXAxis}
+            label={{
+              value: "Frequency (Hz)",
+              position: "insideBottomRight",
+              offset: -10,
+            }}
+          />
+          <YAxis
+            label={{
+              value: "Phase Angle (degrees)",
+              angle: -90,
+              position: "insideLeft",
+            }}
+          />
+          <Tooltip
+            formatter={tooltipFormatter}
+            labelFormatter={tooltipLabelFormatter}
+          />
+          <Legend />
+          <Line type="monotone" dataKey="theta(Rp)" stroke="red" dot={false} />
+          <Line type="monotone" dataKey="theta(Tp)" stroke="blue" dot={false} />
+        </LineChart>
+      </ResponsiveContainer>
+      <h2>Shear Wave Transmission (Ts) and Reflection (Rs)</h2>
+      <p>Absolute ratios to incedents pressure.</p>
+      <ResponsiveContainer width="100%" height={300}>
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="values"
+            tickFormatter={formatXAxis}
+            label={{
+              value: "Frequency (Hz)",
+              position: "insideBottomRight",
+              offset: -10,
+            }}
+          />
+          <YAxis
+            label={{
+              value: "Amplitude (abs)",
+              angle: -90,
+              position: "insideLeft",
+            }}
+          />
+          <Tooltip
+            formatter={tooltipFormatter}
+            labelFormatter={tooltipLabelFormatter}
+          />
+          <Legend />
+          <Line type="monotone" dataKey="abs(Rs)" stroke="green" dot={false} />
+          <Line
+            type="monotone"
+            dataKey="abs(Ts)"
+            stroke="magenta"
+            dot={false}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+      <ResponsiveContainer width="100%" height={300}>
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="values"
+            tickFormatter={formatXAxis}
+            label={{
+              value: "Frequency (Hz)",
+              position: "insideBottomRight",
+              offset: -10,
+            }}
+          />
+          <YAxis
+            label={{
+              value: "Phase Angle (degrees)",
+              angle: -90,
+              position: "insideLeft",
+            }}
+          />
+          <Tooltip
+            formatter={tooltipFormatter}
+            labelFormatter={tooltipLabelFormatter}
+          />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="theta(Rs)"
+            stroke="green"
+            dot={false}
+          />
+          <Line
+            type="monotone"
+            dataKey="theta(Ts)"
+            stroke="magenta"
+            dot={false}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+      <ResponsiveContainer width="100%" height={300}>
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="values"
+            tickFormatter={formatXAxis}
+            label={{
+              value: "Frequency (Hz)",
+              position: "insideBottomRight",
+              offset: -10,
+            }}
+          />
+          <YAxis
+            label={{
+              value: "Phase Angle (degrees)",
+              angle: -90,
+              position: "insideLeft",
+            }}
+          />
+          <Tooltip
+            formatter={tooltipFormatter}
+            labelFormatter={tooltipLabelFormatter}
+          />
+          <Legend />
+          <Line type="monotone" dataKey="RpE" stroke="red" dot={false} />
+          <Line type="monotone" dataKey="TpE" stroke="blue" dot={false} />
+          <Line type="monotone" dataKey="RsE" stroke="green" dot={false} />
+          <Line type="monotone" dataKey="TsE" stroke="magenta" dot={false} />
         </LineChart>
       </ResponsiveContainer>
     </div>
